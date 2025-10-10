e0390d0443da63761d4dc443bd761f452b0dfc30
Babioche <davidlemieux04@gmail.com>
2025-10-10 19:19:20 -0400

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,10 +2,4 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
-{
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-}
-}
+return {}
