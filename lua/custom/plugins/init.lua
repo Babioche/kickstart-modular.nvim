6ef17680404962b0d53bb00b6ea11b5446d372e9
Babioche <davidlemieux04@gmail.com>
2025-10-10 19:43:39 -0400

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,5 +3,5 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  require 'custom.plugins.scottmckendry/"cyberdream.nvim"'
+  require 'custom.plugins.scottmckendry/cyberdream'
 }
