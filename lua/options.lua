d6262a55e1e23081a4e5bc07e6994c71c40bd3d7
Babioche <davidlemieux04@gmail.com>
2025-10-10 12:34:18 -0400

    Update options.lua

--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,8 @@
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+vim.opt.makeprg = 'g++ -Wall -Wextra -g %:p -o %:r'
+
 -- Make line numbers default
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
