8a9ad099b648c2d60a9d761bad6ea4289d70a2e1
Babioche <davidlemieux04@gmail.com>
2025-10-26 10:58:55 -0400

    Set shell option to powershell.exe

--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,7 @@
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 vim.opt.makeprg = 'g++ -Wall -Wextra -g %:p -o %:r'
+vim.o.shell = "powershell.exe"
 
 -- Make line numbers default
 vim.o.number = true
